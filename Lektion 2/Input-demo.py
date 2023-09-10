5ecd1501813a4a2f7e6ff6523641da4579810426
Edvin Jäderlund <edvin.jaderlund@gmail.com>
2023-09-10 16:27:01 +0200

    Lektion 1, 2, 3,commits

---
+++ Lektion 2/Input-demo.py
@@ -0,0 +1,11 @@
+# Inmatning av förstanamn
+FirstName = input("ange ditt namn: ")
+
+# Utmatning av förstanamn
+print(FirstName)
+
+# Inmatning av efternamn
+LastName = input("ange efternamn: ")
+
+# Utmatning av efternamn
+print("Hej", FirstName, LastName)
---
+++ Lektion 2/casting.py
@@ -0,0 +1,6 @@
+#Operatorer
+
+myBill = float(input("What was the bill?: "))
+numberOfPeople = input("How many people: ")
+answer = float(myBill) / float(numberOfPeople)
+print("You all owe", round(answer))
