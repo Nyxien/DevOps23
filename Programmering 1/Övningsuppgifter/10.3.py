e912b2e005fa26c98af51f8016375d3772ba56ff
Edvin Jäderlund <edvin.jaderlund@gmail.com>
2023-09-11 09:04:30 +0200

    Merg commits

---
+++ Programmering 1/Övningsuppgifter/10.3.py
@@ -0,0 +1,14 @@
+# Funktion för lista av människor
+def läs_list():
+    try:
+        with open(r'C:\Users\edvin\Downloads\database.csv', 'r') as file:
+            innehåll = file.readlines()
+            return [line.strip().split(', ') for line in innehåll]
+
+    except FileNotFoundError:
+        print("Error, fil ej hittad")
+
+# Funktion för att söka efter människor
+def sök_list():
+    sökID_list = input("")
+
